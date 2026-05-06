ca92d67c845428d56a1418ea6f4c445b86940ec9
Oleh Pona <olehpona@gmail.com>
2026-05-06 13:28:25 +0300

    update

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -119,7 +119,7 @@ resource "aws_ecs_service" "app_service" {
 resource "aws_iam_openid_connect_provider" "github" {
   client_id_list  = ["sts.amazonaws.com"]
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
 
 resource "aws_iam_role" "github_action_role" {
